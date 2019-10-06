4e1a873134c6b86f4535244de13dd52d52f39d7b
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-10-06 21:01:07 +0300

    Add function on  js

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,45 @@ $(document).ready(function() {
         $("#hide3").toggle();
     });
 });
+function Details(type, size, crust, toppings, quantity){
+    this.selectedType = type;
+    this.selectedSize = size;
+    this.selectedCrust = crust;
+    this.selectedToppings = toppings;
+    this.selectedQuantity = quantity;
+  }
+  
+  function Total(price, quantity,){
+    this.price = price;
+    this.quantity = quantity;
+  }
+  
+  Total.prototype.finalTotal = function(){
+    return this.price * this.quantity ;
+  }
+  
+  
+  $(document).ready(function(){
+    $('#form2').submit(function(event){
+      event.preventDefault();
+  
+      var sizePrices = [ small="500",medium="750",large="1500"];
+      var inputtedType = $('#type').val();
+      var inputtedCrust = $('#crust').val();
+      var inputtedToppings= $('#toppings').val();
+      var inputtedSize = parseInt($('#size').val());
+      var pizzaPrice = sizePrices[inputtedSize-1];
+      var inputtedQuantity = parseInt($('#quantity').val());
+  
+      var newDetails = new Details(inputtedType, inputtedSize, inputtedCrust, inputtedToppings, inputtedQuantity);
+      var newTotal = new Total(pizzaPrice, inputtedQuantity);
+      var newBill = newTotal.finalTotal();
+  
+  
+      alert("your order is " + newDetails.selectedType + ", " + newDetails.selectedToppings + ', and ' + newDetails.selectedCrust +". click ok to view your bill");
+      alert(newBill +"");
+  
+      document.getElementById('form2').reset();
+  
+    });
+  });
